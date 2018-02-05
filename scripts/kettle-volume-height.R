aec14b2d67fce4f88f7e8d10fe24bfc6d17d0746
Jean-Philippe Barrette-LaPierre <jpb@rrette.com>
2018-02-05 19:26:21 +0100

    Added linear regression

--- scripts/kettle-volume-height.R
+++ scripts/kettle-volume-height.R
@@ -5,3 +5,9 @@ ggplot(dat, aes(x=xvar, y=yvar)) +
   geom_point(shape=1) +    # Use hollow circles
   geom_smooth(method=lm)   # Add linear regression line 
 #  (by default includes 95% confidence region)
+
+# shows the linear regression
+lm(yvar ~ xvar, dat)
+
+kettle_volume_height <- function(x) { -0.7059 + 0.9409 * x }
+kettle_volume_height(15.9)
